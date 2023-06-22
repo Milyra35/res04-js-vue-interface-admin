8cd30fbb5b5bbcc17038a691024458c7babf3621
Milyra35 <marie.richir35@gmail.com>
2023-06-22 12:24:07 +0200

    début mini projet interface admin

---
+++ assets/js/App.js
@@ -0,0 +1,16 @@
+export default {  
+    data() {  
+        return {  
+            
+        }  
+    },  
+    methods : {  
+         putActiveCLass() 
+         {
+            if (window.location.pathname === "./index.html")
+            {
+                
+            }
+         }
+    }  
+}
